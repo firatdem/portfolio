88b12a430390318025ea08716f3ee75cca6ee137
firatdem <145790880+firatdem@users.noreply.github.com>
2023-10-17 13:57:01 -0400

    Update BFS Search.cpp

--- BFS Search/BFS Search/BFS Search.cpp
+++ BFS Search/BFS Search/BFS Search.cpp
@@ -1,7 +1,4 @@
 // BFS Search.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-// AI assignment 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
 
